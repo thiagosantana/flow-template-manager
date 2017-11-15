20c76f15d98f72e7dc533243aca057d0cd0a849a
Thiago Santana <thiago.santana@gmail.com>
2017-11-14 22:07:26 -0200

    static resources

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static());
 app.get('/', (req, res) => res.send('Wooza template generator'));
 
 app.listen(3000, () => console.log('App Listening on port 3000'));
