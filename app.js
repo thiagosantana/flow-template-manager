58732082258f5d690e4cd60a79cda0d748c3f56b
Thiago Santana <thiago.santana@gmail.com>
2017-11-14 22:11:46 -0200

    static resources fix

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static());
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.send('Wooza template generator'));
 
 app.listen(3000, () => console.log('App Listening on port 3000'));
