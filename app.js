6ad1b9ab26a67c29bbbfdc1353636ccbe6be32a6
Thiago Santana <thiago.santana@gmail.com>
2017-11-14 22:25:16 -0200

    express pug integration

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.send('Wooza template generator'));
+app.set('views', './templates');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Wooza Flow Template Manager',
+        message: 'V0.0.1'
+    });
+});
 
 app.listen(3000, () => console.log('App Listening on port 3000'));
